deb5293805420515bca9a5c7808d5636ad75bcd9
cara marie <cara.marie@me.com>
2013-08-01 14:00:10 -0700

    unmodified, capslock & shift maps

--- keymap/build.py
+++ keymap/build.py
@@ -5,31 +5,29 @@ import linecache
 import fnmatch
 import re
 
-special = ['RESERVED', 'ESC', 'BACKSPACE', 'ENTER', 
-'CAPSLOCK', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 
-'F7', 'F8', 'F9', 'F10', 'NUMLOCK', 'SCROLLLOCK', 'F11','F12', 
-'SYSRQ', 'LINEFEED', 'UP', 'PAGEUP', 'LEFT', 'RIGHT', 'END', 'KPENTER',
-'DOWN', 'PAGEDOWN', 'INSERT', 'DELETE', 'MACRO', 'MUTE', 'VOLUMEDOWN', 
-'VOLUMEUP', 'POWER', 'PAUSE', 'SCALE', 'HANGEUL', 
-'COMPOSE', 'STOP', 'AGAIN', 'PROPS', 'UNDO', 'FRONT', 'COPY', 'OPEN', 
-'PASTE', 'FIND', 'CUT', 'HELP', 'MENU', 'CALC', 'SETUP', 'SLEEP', 'WAKEUP', 
-'FILE', 'SENDFILE', 'DELETEFILE', 'XFER', 'PROG1', 'PROG2', 'WWW', 'MSDOS', 
-'COFFEE', 'SCREENLOCK', 'DIRECTION', 'CYCLEWINDOWS', 'MAIL', 'BOOKMARKS', 
-'COMPUTER', 'BACK', 'FORWARD', 'CLOSECD', 'EJECTCD', 'EJECTCLOSECD', 'HOME',
-'NEXTSONG', 'PLAYPAUSE', 'PREVIOUSSONG', 'STOPCD', 'RECORD', 'REWIND', 
-'PHONE', 'ISO', 'CONFIG', 'HOMEPAGE', 'REFRESH', 'EXIT', 'MOVE',
-'EDIT', 'SCROLLUP', 'SCROLLDOWN', 'NEW', 'REDO', 'F13', 'F14', 'F15', 'F16', 
-'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', 'PLAYCD', 'PAUSECD', 
-'PROG3', 'PROG4', 'DASHBOARD', 'SUSPEND', 'CLOSE', 'PLAY', 'FASTFORWARD', 
-'BASSBOOST', 'PRINT', 'HP', 'CAMERA', 'SOUND', 'QUESTION', 'EMAIL', 'CHAT', 
-'SEARCH', 'CONNECT', 'FINANCE', 'SPORT', 'SHOP', 'ALTERASE', 'CANCEL', 
-'BRIGHTNESSDOWN', 'BRIGHTNESSUP', 'MEDIA', 'SWITCHVIDEOMODE', 'KBDILLUMTOGGLE', 
-'KBDILLUMDOWN', 'KBDILLUMUP', 'SEND', 'REPLY', 'FORWARDMAIL', 'SAVE', 
-'DOCUMENTS', 'BATTERY', 'BLUETOOTH', 'WLAN', 'UWB', 'UNKNOWN', 'VIDEO_NEXT', 
-'VIDEO_PREV', 'BRIGHTNESS_CYCLE', 'BRIGHTNESS_ZERO', 'DISPLAY_OFF', 'WIMAX', 
-'RFKILL']
+special = ['RESERVED', 'ESC', 'BACKSPACE',  'CAPSLOCK', 'F1', 'F2', 'F3', 
+'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'NUMLOCK', 'SCROLLLOCK', 'F11',
+'F12', 'SYSRQ', 'LINEFEED', 'UP', 'PAGEUP', 'LEFT', 'RIGHT', 'END', 'DOWN', 
+'PAGEDOWN', 'INSERT', 'DELETE', 'MACRO', 'MUTE', 'VOLUMEDOWN', 'VOLUMEUP', 
+'POWER', 'PAUSE', 'SCALE', 'HANGEUL', 'COMPOSE', 'STOP', 'AGAIN', 'PROPS', 
+'UNDO', 'FRONT', 'COPY', 'OPEN', 'PASTE', 'FIND', 'CUT', 'HELP', 'MENU', 
+'CALC', 'SETUP', 'SLEEP', 'WAKEUP', 'FILE', 'SENDFILE', 'DELETEFILE', 'XFER', 
+'PROG1', 'PROG2', 'WWW', 'MSDOS', 'COFFEE', 'SCREENLOCK', 'DIRECTION', 
+'CYCLEWINDOWS', 'MAIL', 'BOOKMARKS', 'COMPUTER', 'BACK', 'FORWARD', 
+'CLOSECD', 'EJECTCD', 'EJECTCLOSECD', 'HOME','NEXTSONG', 'PLAYPAUSE', 
+'PREVIOUSSONG', 'STOPCD', 'RECORD', 'REWIND', 'PHONE', 'ISO', 'CONFIG', 
+'HOMEPAGE', 'REFRESH', 'EXIT', 'MOVE','EDIT', 'SCROLLUP', 'SCROLLDOWN', 
+'NEW', 'REDO', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 
+'F22', 'F23', 'F24', 'PLAYCD', 'PAUSECD', 'PROG3', 'PROG4', 'DASHBOARD', 
+'SUSPEND', 'CLOSE', 'PLAY', 'FASTFORWARD', 'BASSBOOST', 'PRINT', 'HP', 
+'CAMERA', 'SOUND', 'QUESTION', 'EMAIL', 'CHAT', 'SEARCH', 'CONNECT', 
+'FINANCE', 'SPORT', 'SHOP', 'ALTERASE', 'CANCEL', 'BRIGHTNESSDOWN', 
+'BRIGHTNESSUP', 'MEDIA', 'SWITCHVIDEOMODE', 'KBDILLUMTOGGLE', 'KBDILLUMDOWN', 
+'KBDILLUMUP', 'SEND', 'REPLY', 'FORWARDMAIL', 'SAVE', 'DOCUMENTS', 'BATTERY', 
+'BLUETOOTH', 'WLAN', 'UWB', 'UNKNOWN', 'VIDEO_NEXT', 'VIDEO_PREV', 
+'BRIGHTNESS_CYCLE', 'BRIGHTNESS_ZERO', 'DISPLAY_OFF', 'WIMAX', 'RFKILL']
 
-sym = {'TAB': ' [TAB]	', 'RIGHTCTRL': '[CTRL]', 'LEFTCTRL': '[CTRL]', 
+sym = {'ENTER': ' [ENTER]\n', 'KPENTER': ' [ENTER]\n', 'TAB': ' [TAB]	', 'RIGHTCTRL': '[CTRL]', 'LEFTCTRL': '[CTRL]', 
 'MINUS': '-', 'EQUAL': '=', 'LEFTBRACE': '[', 'RIGHTBRACE': ']', 
 'SEMICOLON': ';', 'APOSTROPHE': "'", 'GRAVE': '`', 'RIGHTALT': '[ALT]',
 'BACKSLASH': '\\', 'COMMA': ',', 'DOT': '.', 'SLASH': '/', 'KPASTERISK': '*', 
@@ -43,7 +41,17 @@ sym = {'TAB': ' [TAB]	', 'RIGHTCTRL': '[CTRL]', 'LEFTCTRL': '[CTRL]',
 'G': 'g', 'H': 'h', 'J': 'j', 'K': 'k', 'L': 'l', 'Z': 'z', 'X': 'x', 
 'C': 'c', 'V': 'v', 'B': 'b', 'N': 'n', 'M': 'm'}
 
-shift_sym = {'TAB': ' [TAB]	', 'RIGHTCTRL': '[CTRL]', 'LEFTCTRL': '[CTRL]', 
+cap_sym = {'ENTER': ' [ENTER]\n', 'KPENTER': ' [ENTER]\n', 'TAB': ' [TAB]	', 'RIGHTCTRL': '[CTRL]', 'LEFTCTRL': '[CTRL]', 
+'MINUS': '-', 'EQUAL': '=', 'LEFTBRACE': '[', 'RIGHTBRACE': ']', 
+'SEMICOLON': ';', 'APOSTROPHE': "'", 'GRAVE': '`', 'RIGHTALT': '[ALT]',
+'BACKSLASH': '\\', 'COMMA': ',', 'DOT': '.', 'SLASH': '/', 'KPASTERISK': '*', 
+'SPACE': ' ', 'KP7': '7', 'KP8': '8', 'KP9': '9', 'KPMINUS': '-', 'KP4': '4', 
+'KP5': '5', 'KP6': '6', 'KPPLUS': '+', 'KP1': '1', 'KP2': '2', 'KP3': '3', 
+'KP0': '0', 'KPDOT': '.', 'KPSLASH': '/', 'KPEQUAL': '=', 'KPPLUSMINUS': '+-', 
+'KPCOMMA': ',', 'LEFTMETA': 'COMMAND', 'RIGHTMETA': 'COMMAND', 
+'LEFTSHIFT': '[SHIFT]', 'RIGHTSHIFT': '[SHIFT]', 'LEFTALT': '[ALT]'}
+
+shift_sym = {'ENTER': ' [ENTER]\n', 'KPENTER': ' [ENTER]\n', 'TAB': ' [TAB]	', 'RIGHTCTRL': '[CTRL]', 'LEFTCTRL': '[CTRL]', 
 'MINUS': '_', 'EQUAL': '+', 'LEFTBRACE': '{', 'RIGHTBRACE': '}', 
 'SEMICOLON': ':', 'APOSTROPHE': '"', 'GRAVE': '~', 'RIGHTALT': '[ALT]',
 'BACKSLASH': '|', 'COMMA': '<', 'DOT': '>', 'SLASH': '?', 'KPASTERISK': '*', 
@@ -56,7 +64,25 @@ shift_sym = {'TAB': ' [TAB]	', 'RIGHTCTRL': '[CTRL]', 'LEFTCTRL': '[CTRL]',
 '9': '(', '0': ')'}
 
 keymap = []
+cap_keymap = []
 shift_keymap = []
+
+def keyed(pyf, km, sym_list):
+	for s in km:
+		if s == km[len(km)-1]:
+			last = "'[" + s + "]']"
+			pyf.write(last)
+			break
+		elif s in special:
+			btn = "'[" + s + "]',\n"
+			pyf.write(btn)
+		elif s in sym_list:
+			punc = "'" + sym_list[s] + "',\n"
+			pyf.write(punc)
+		else:
+			entry = "'" + s + "', \n"
+			pyf.write(entry)
+	return km
 
 for i in range(183, 442):
 	linput = linecache.getline("linux-input.h", i).split()
@@ -64,43 +90,20 @@ for i in range(183, 442):
 	try:
 		key = re.sub(r'KEY_', "", key[0])
 		keymap.append(key)
+		cap_keymap.append(key)
 		shift_keymap.append(key)
 	except:
 		continue
 
 f = open("keymap.py", "w")
-f.write("keys = [\n")
 
-for k in keymap:
-	if k == keymap[len(keymap)-1]:
-		last = "'[" + k + "]']"
-		f.write(last)
-		break
-	elif k in special:
-		btn = "'[" + k + "]',\n"
-		f.write(btn)
-	elif k in sym:
-		punc = "'" + sym[k] + "',\n"
-		f.write(punc)
-	else:
-		entry = "'" + k + "', \n"
-		f.write(entry)
+f.write("keys = [\n")  				# Create unmodified keymap.
+keyed(f, keymap, sym)
 
-f.write("\n\nshift_keys = [\n")
+f.write("\n\ncap_keys = [\n") 		# Create capslock keymap.
+keyed(f, cap_keymap, cap_sym)
 
-for s in shift_keymap:
-	if s == shift_keymap[len(shift_keymap)-1]:
-		last = "'[" + s + "]']"
-		f.write(last)
-		break
-	elif s in special:
-		btn = "'[" + s + "]',\n"
-		f.write(btn)
-	elif s in shift_sym:
-		punc = "'" + shift_sym[s] + "',\n"
-		f.write(punc)
-	else:
-		entry = "'" + s + "', \n"
-		f.write(entry)
+f.write("\n\nshift_keys = [\n")		# Create shift keymap.
+keyed(f, shift_keymap, shift_sym)
 
 f.close()
