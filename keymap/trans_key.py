79526a88bc855e01b7eb37f86d6a16227fd3e013
cara marie <cara.marie@me.com>
2013-08-01 19:18:42 -0700

    shift_list working in conjunction with keys

--- keymap/trans_key.py
+++ keymap/trans_key.py
@@ -1,5 +1,6 @@
 from keymap import keys, cap_keys, shift_keys
 
+shift = False
 evlst = []
 
 log = open('evlog.txt')
@@ -22,7 +23,7 @@ for ev in evlog:
 # num = for loop iterator, km = keymap, kl = keylog, key_sym = key symbol
 def counter(num, km, kl, key_sym):
 	if km[kl[num][0]] != key_sym:
-		return 2
+		return 0
 	return 2 + counter(num-1, km, kl, key_sym)
 
 
@@ -33,20 +34,42 @@ def del_count(num, c, kl):
 	del_count(num-1, c-1, kl)
 
 
+# YOU ARE TRAVERSING IN REVERSE REVERSE REVERSE!!!!
 for n in reversed(xrange(len(evlst))):
-	# delete depressed state
-	if evlst[n][1] == 1 and evlst[n][1] is not '[SHIFT]':
+	if keys[evlst[n][0]] == '[SHIFT]' and evlst[n][1] == 0:
+		shift = True
 		del evlst[n]
 		continue
+	elif keys[evlst[n][0]] == '[SHIFT]' and evlst[n][1] == 1:
+		shift = False
+		del evlst[n]
+		continue
+
+	if shift:
+		if evlst[n][1] == 0:
+			del evlst[n]
+			continue
+		evlst[n] = shift_keys[evlst[n][0]]
+	else:
+		if evlst[n][1] == 0:
+			del evlst[n]
+			continue
+		evlst[n] = keys[evlst[n][0]]
+
+	# if keys[evlst[n][0]] == '[ENTER]':
+	# 	evlst[n] = keys[evlst[n][0]] + '\n'
+	# if evlst[n][1] == 1 and keys[evlst[n][0]] == '[SHIFT]':
+	# 	while True:
+	# 		if evlst[n][1] == 0 and keys[evlst[n][0]] != '[SHIFT]':
+	# 			evlst[n] = shift_keys[evlst[n][0]]
+
+	# delete depressed state
 	# function for backspace
 	# count the number of backspaces and then delete that number of 
 	# indices above
-	if keys[evlst[n][0]] == '[BACKSPACE]':
-		c = counter(n, keys, evlst, '[BACKSPACE]')
-		del_count(n, c, evlst)
-		continue
+	# if keys[evlst[n][0]] == '[BACKSPACE]':
+	# 	c = counter(n, keys, evlst, '[BACKSPACE]')
+	# 	del_count(n, c, evlst)
 
-
-	evlst[n] = keys[evlst[n][0]]
 # print evlst
-# print ''.join(evlst)
+print ''.join(evlst)
